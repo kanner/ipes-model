3834a60358bf314cc331b77e240c83d453b54991
Andrew Kanner <andrew.kanner@gmail.com>
2020-09-01 12:42:03 +0300

    contrib: fixed verification script, added results of model checking (-dfid 6)

--- contrib/model-check.sh
+++ contrib/model-check.sh
@@ -2,4 +2,6 @@
 set -ex
 #docker pull akanner/centos7:tla-bin_tex
 docker run -it --rm -v `git rev-parse --show-toplevel`:/ipes-model akanner/centos7:tla-bin_tex /bin/bash -c \
-	"cd /ipes-model && time tlc -workers 6 -coverage 25 -dfid 6 ipes.tla > ipes-model-check.log 2>&1"
+	"cd /ipes-model && time tlc -workers 6 -dfid 6 ipes.tla > result-dfid6.log 2>&1"
+#	"cd /ipes-model && time tlc -workers 6 -dfid 8 ipes.tla > result-dfid8.log 2>&1"
+#	"cd /ipes-model && time tlc -workers 6 -coverage 25 -dfid 6 ipes.tla > ipes-model-check.log 2>&1"
