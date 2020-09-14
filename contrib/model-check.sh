b7cf91dafd73edbd9b7e470fa0a62a4b4b148096
Andrew Kanner <andrew.kanner@gmail.com>
2020-09-14 10:11:08 +0300

    contrib: fixed script, added results of model checking (-dfid 8, java oom)

--- contrib/model-check.sh
+++ contrib/model-check.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 #docker pull akanner/centos7:tla-bin_tex
-docker run -it --rm -v `git rev-parse --show-toplevel`:/ipes-model akanner/centos7:tla-bin_tex /bin/bash -c \
-	"cd /ipes-model && time tlc -workers 6 -dfid 6 ipes.tla > result-dfid6.log 2>&1"
-#	"cd /ipes-model && time tlc -workers 6 -dfid 8 ipes.tla > result-dfid8.log 2>&1"
-#	"cd /ipes-model && time tlc -workers 6 -coverage 25 -dfid 6 ipes.tla > ipes-model-check.log 2>&1"
+docker run -it --rm -v `git rev-parse --show-toplevel`:/ipes-model \
+	-e JAVA_OPTS='-XX:+IgnoreUnrecognizedVMOptions -XX:+UseParallelGC -d64 -Xmx10g -Xms1g -Xss1g -XX:MaxPermSize=10g' \
+	akanner/centos7:tla-bin_tex /bin/bash -c \
+	"cd /ipes-model && tlc -workers 6 -dfid 6 ipes.tla > result-dfid6.log 2>&1"
+#	"cd /ipes-model && tlc -workers 6 -dfid 7 ipes.tla > result-dfid7.log 2>&1"
+#	"cd /ipes-model && tlc -workers 6 -coverage 25 -dfid 6 ipes.tla > ipes-model-check.log 2>&1"
